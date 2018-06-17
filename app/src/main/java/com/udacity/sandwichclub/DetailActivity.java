2b29bc85b460fcef346c98003f399bd84deed4ce
Brendan <brendan@Brendans-MacBook-Pro.local>
2018-06-17 16:39:15 -0400

    Finished activity xml and the detailactivity class

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,28 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.EventLogTags;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView descriptionText;
+    private TextView placeOfOriginText;
+    private TextView akaText;
+    private TextView ingredientsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +67,47 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        //Set place of origin
+        placeOfOriginText = findViewById(R.id.origin_tv);
+        String origin = sandwich.getPlaceOfOrigin();
+        if(origin != null && origin != ""){
+            placeOfOriginText.setText(origin);
+        } else {
+            placeOfOriginText.setText("N/A");
+        }
 
+        //Set Also Known As
+        akaText = findViewById(R.id.also_known_tv);
+        List<String> akaList = sandwich.getAlsoKnownAs();
+        if(akaList != null && !akaList.isEmpty()){
+            for(String aka : akaList){
+                akaText.append((aka) + "\n\n");
+            }
+            akaText.setText(akaText.getText().toString().trim());
+        } else {
+            akaText.setText("Other names not available!");
+        }
+
+        //Set Description
+        descriptionText = findViewById(R.id.description_tv);
+        String description = sandwich.getDescription();
+        if (description != null && description != ""){
+            descriptionText.setText(description);
+        } else {
+            descriptionText.setText("No description available!");
+        }
+
+        //Set ingredients
+        ingredientsText = findViewById(R.id.ingredients_tv);
+        List<String> ingredientsList = sandwich.getIngredients();
+        if(ingredientsList != null && !ingredientsList.isEmpty()){
+            for(String ingredient : ingredientsList){
+                ingredientsText.append((ingredient) + "\n\n");
+            }
+            ingredientsText.setText(ingredientsText.getText().toString().trim());
+        } else {
+            akaText.setText("Ingredients not available");
+        }
     }
 }
